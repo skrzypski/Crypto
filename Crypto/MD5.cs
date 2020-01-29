a105f10779e31b7e9e7baa8d2ef23b6ab2a0ff9f
Kacper Skrzypek <kac.pek.3@hotmail.com>
2020-01-29 09:02:08 +0100

    Finished MD5 class

--- Crypto/MD5.cs
+++ Crypto/MD5.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
+using System.Security.Cryptography;
 
 namespace Crypto
 {
-    class MD5
+    static class MD5
     {
+        static string Generate(string input)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
     }
 }
