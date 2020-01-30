58c9618ad9c79dcbea1cb0e6e4e727ebba94ed2c
Kacper Skrzypek <kac.pek.3@hotmail.com>
2020-01-30 12:40:43 +0100

    Added Cesar.cs class

---
+++ Crypto/Cesar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    class Cesar
+    {
+        public int shift { get; private set; }
+
+        public Cesar(int shift)
+        {
+            this.shift = shift;
+        }
+
+        public string Encode(string text)
+        {
+            char[] arr = text.ToCharArray();
+            List<char> encoded = new List<char>();
+            foreach(char ch in arr)
+            {
+                char tmp = (char)((int)(ch) + shift);
+                encoded.Add(tmp);
+            }
+            return new string(encoded.ToArray());
+        }
+
+        public string Decode(string text)
+        {
+            char[] arr = text.ToCharArray();
+            List<char> encoded = new List<char>();
+            foreach (char ch in arr)
+            {
+                char tmp = (char)((int)(ch) - shift);
+                encoded.Add(tmp);
+            }
+            return new string(encoded.ToArray());
+        }
+    }
+}
